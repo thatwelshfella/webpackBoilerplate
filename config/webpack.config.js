e13bd5890026bc3fdde008ed7beb5cdfb3aac7a4
Luke Delaney <delaney.luke95@gmail.com>
2020-01-16 21:37:58 +0000

    feat(webpack): webpack now accepts module scss files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './dist/index.html',
   filename: 'index.html',
   inject: 'body',
-  publicPath:'/'
+  publicPath: '/',
+});
+
+const MiniCssExtractPluginConfig = new MiniCssExtractPlugin({
+  filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+  chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
 });
 
 module.exports = {
@@ -16,12 +24,13 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
     filename: 'bundle.js',
-  },  devServer: {
+  },
+  devServer: {
     contentBase: '../dist',
   },
   resolve: {
     modules: ['src', 'node_modules'],
-    extensions: ['.js', '.jsx', '.less'],
+    extensions: ['.js', '.jsx', '.less', '.scss'],
   },
   module: {
     rules: [
@@ -49,7 +58,40 @@ module.exports = {
         exclude: '/node_modules',
         loader: 'url-loader',
       },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [HtmlWebpackPluginConfig, MiniCssExtractPluginConfig],
 };
